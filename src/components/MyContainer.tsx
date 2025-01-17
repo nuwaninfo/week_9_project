0a27389bc6a41767a93226541444c56a5a024b21
Nuwan Fernando <nuwaninfo@gmail.com>
2025-01-18 01:19:20 +0200

    Done 4th part of week 9 exercise

--- src/components/MyContainer.tsx
+++ src/components/MyContainer.tsx
@@ -1,4 +1,5 @@
 import MyList from "./MyList"
+import { useState } from "react"
 
 type TItem = {
   id: string
@@ -7,14 +8,32 @@ type TItem = {
 
 const MyContainer = () => {
   const header: string = "This is a title header"
-  const items: TItem[] = [
+  const [items, setItems] = useState<TItem[]>([
     { id: "1", text: "This is first task" },
     { id: "2", text: "This is second task" },
-  ]
+  ])
+  const [newText, setNewText] = useState<string>("")
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    const id: string = `${Date.now()}-${Math.random()
+      .toString(36)
+      .substr(2, 9)}`
+    const newItem: TItem = { id, text: newText }
+    setItems([...items, newItem])
+    setNewText("")
+  }
 
   return (
     <div>
       <MyList header={header} items={items} />
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={newText}
+          onChange={(e) => setNewText(e.target.value)}
+        ></textarea>
+        <button type="submit">Add text</button>
+      </form>
     </div>
   )
 }
