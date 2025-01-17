1b794853e6e966035438150ecf172977ac52b07b
Nuwan Fernando <nuwaninfo@gmail.com>
2025-01-17 23:45:14 +0200

    Done 3rd task of week 9 exercise

--- src/components/MyContainer.tsx
+++ src/components/MyContainer.tsx
@@ -1,9 +1,20 @@
 import MyList from "./MyList"
 
+type TItem = {
+  id: string
+  text: string
+}
+
 const MyContainer = () => {
+  const header: string = "This is a title header"
+  const items: TItem[] = [
+    { id: "1", text: "This is first task" },
+    { id: "2", text: "This is second task" },
+  ]
+
   return (
     <div>
-      <MyList />
+      <MyList header={header} items={items} />
     </div>
   )
 }
--- src/components/MyList.tsx
+++ src/components/MyList.tsx
@@ -1,5 +1,24 @@
-const MyList = () => {
-  return <div></div>
+interface ListProps {
+  header: string
+  items: TItem[]
+}
+
+type TItem = {
+  id: string
+  text: string
+}
+
+const MyList: React.FC<ListProps> = ({ header, items }) => {
+  return (
+    <div>
+      <h1>{header}</h1>
+      <ol>
+        {items.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ol>
+    </div>
+  )
 }
 
 export default MyList
